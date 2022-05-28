3c79136eface8126e35bc12c8126f5b2456603f5
Mike Sanders <msanders331@gmail.com>
2022-05-27 20:43:39 -0700

    updated the contact.js file. Now has full functionality after testing.

--- src/Components/ContactMe/Contact.js
+++ src/Components/ContactMe/Contact.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 export default function Contact() {
-    // Create state variables for the fields in the form
-    function validateEmail(email) {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-      }
+  // Create state variables for the fields in the form
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
   // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
@@ -18,13 +18,11 @@ export default function Contact() {
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
+    // Based on the input type, we set the state of either email or full name
+    if (inputType === "email") {
       setEmail(inputValue);
-    } else if (inputType === 'userName') {
-      setUserName(inputValue);
-    } else {
-      setPassword(inputValue);
+    } else if (inputType === "userName") {
+      setFullName(inputValue);
     }
   };
 
@@ -33,29 +31,23 @@ export default function Contact() {
     e.preventDefault();
 
     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
+    if (!validateEmail(email) || !fullName) {
+      setErrorMessage("Email or username is invalid");
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
       // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
-    // if (!checkPassword(password)) {
-    //   setErrorMessage(
-    //     `Choose a more secure password for the account: ${userName}`
-    //   );
-    //   return;
-    // }
-    alert(`Hello ${userName}`);
+
+    alert(`Hello ${fullName}`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setPassword('');
-    setEmail('');
+    setFullName("");
+    setEmail("");
   };
 
   return (
     <div>
-      <p>Hello {userName}</p>
+      <p>Hello {fullName}</p>
       <form className="form">
         <input
           value={email}
@@ -65,20 +57,16 @@ export default function Contact() {
           placeholder="email"
         />
         <input
-          value={userName}
+          value={fullName}
           name="userName"
           onChange={handleInputChange}
           type="text"
           placeholder="username"
         />
-        <input
-          value={password}
-          name="password"
-          onChange={handleInputChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
+
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
       </form>
       {errorMessage && (
         <div>
@@ -88,4 +76,3 @@ export default function Contact() {
     </div>
   );
 }
-
