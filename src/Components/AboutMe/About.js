c640a6e1b945e49807240e3b7fafd1d3fae70a94
Mike Sanders <msanders331@gmail.com>
2022-05-29 18:13:23 -0700

    updated the about me and skills components with conteent

--- src/Components/AboutMe/About.js
+++ src/Components/AboutMe/About.js
@@ -4,7 +4,8 @@ export default function About () {
     return (
         <>
             <h1>About Me</h1>
-            <p></p>
+            <p>I'm a web developer with a background in project management and Saas software implementation.</p>
+            <p>My favorite past times include spending time with my beautiful wife Tatiana and my adorable daughter Alice. I'm a passionate puzzle hunter and love a good puzzle game, escape room or riddle.</p>
         </>
     )
 }
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,7 +1,47 @@
 import React from 'react'
 
-export default function About () {
+export default function Skills () {
     return (
-        <h1>About Me</h1>
+        <>
+            <ul>
+                <li>Front End:
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>JQuery</li>
+                    </ul>
+                </li>
+                <li>Back End:
+                    <ul>
+                        <li>NodeJS</li>
+                        <li>HandleBars</li>
+                        <li>WebPack</li>
+                        <li>Sequel</li>
+                        <li>Sequelize</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                        <li>GraphQL with Apollo</li>
+                    </ul>
+                </li>
+                <li>Developer Tools:
+                    <ul>
+                        <li>Git/GitHub</li>
+                        <li>Heroku</li>
+                        <li>Postman</li>
+                        <li>VSCode</li>
+                    </ul>
+                </li>
+                <li>Other Software Tools:
+                    <ul>
+                        <li>Smartsheet</li>
+                        <li>Salesforce</li>
+                        <li>JIRA</li>
+                        <li>JIRA</li>
+                    </ul>
+                </li>
+            </ul>
+        </>
     )
 }
